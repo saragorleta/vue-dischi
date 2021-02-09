ae575f029bc16f49ec4825869d5bd361080b681e
Sara <saragorleta@yahoo.com>
2021-02-09 15:35:59 +0100

    gf

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ var app = new Vue({
     axios
     get('https://flynn.boolean.careers/exercises/api/array/music')
     then((result)=> {
-      this.albums =result.data.entries;
+      this.albums =result.data.response;
 
       this.albums.forEach(element, index) =>{
         if(!this.generi.includes(element.genre)){
