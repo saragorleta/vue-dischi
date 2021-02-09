f8aa5cd40f109c2d0e187974b274ec49e0139962
Sara <saragorleta@yahoo.com>
2021-02-09 15:27:07 +0100

    aggiunto ciclo for per stamapre foto, titolo e anno

---
+++ js/script.js
@@ -0,0 +1,20 @@
+var app = new Vue({
+  el:'#app',
+  data: {
+    albums:[],
+    generi:[]
+  },
+  mounted(){
+    axios
+    get('https://flynn.boolean.careers/exercises/api/array/music')
+    then((result)=> {
+      this.albums =result.data.entries;
+
+      this.albums.forEach(element, index) =>{
+        if(!this.generi.includes(element.genre)){
+          this.generi.push(element.genre);
+        }
+      }
+    });
+  }
+});
